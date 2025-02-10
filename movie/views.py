15f66100c8289ff1e30d15fe97fbdcd96e7c99d7
Jacobogz <jgiraldoz@eafit.edu.co>
2025-02-10 11:49:32 -0500

    Primer commit

---
+++ movie/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+from .models import Movie
+
+# Create your views here.
+def home(request):
+    searchTerm = request.GET.get('searchMovie')
+    movies = Movie.objects.all()
+    return render(request, 'home.html', {'movies': movies})
+
+def about(request):
+    return HttpResponse("About this site")
